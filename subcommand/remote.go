304e8a40d5da8c904594cdd3518622ee931de944
たけちゃん <79554611+takechan-NITNC@users.noreply.github.com>
2022-12-17 02:58:59 +0900

    remoteの完成

--- subcommand/remote.go
+++ subcommand/remote.go
@@ -1,5 +1,16 @@
 package subcommand
 
-func Remote(args []string) {
+import (
+	"flag"
+	"fmt"
+)
 
+func Remote(args []string) {
+	var flags = flag.NewFlagSet("remote", flag.ExitOnError)
+	flags.Parse(args)
+	if flags.Args()[0] == "add" {
+		RemoteAdd(args[1:])
+	} else {
+		panic(fmt.Errorf("未知のサブコマンド「%s」", flags.Args()[0]))
+	}
 }
